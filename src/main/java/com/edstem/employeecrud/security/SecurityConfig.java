6ea06d86d6287798b070df0d75745815ea694d3b
shyamkumarpv <shyam.kumar@edstem.com>
2023-07-24 12:53:57 +0530

    added security

---
+++ src/main/java/com/edstem/employeecrud/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.edstem.employeecrud.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(auth -> auth.anyRequest().authenticated());
+
+        http.httpBasic(withDefaults());
+
+        http.csrf().disable();
+        return http.build();
+    }
+}
